d84b7ab2f7883cf8f1fa1346820b4514cd7aa824
Ebereonwu Einstein Munachiso <einsteinmunachiso@gmail.com>
2025-03-26 13:37:00 +0000

    Updated: Quickstart & Python SDK

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
 api_key = "wtc-sk-ad74a24c0976d994b382a85c48b31f6d79288983"
 
-from wetro import Wetrocloud
+import asyncio
+from wetro import AsyncWetrocloud
 
-client = Wetrocloud(api_key=api_key)
-tools_client = client.tools
+async def main():
+    client = AsyncWetrocloud(api_key=api_key)
+    response = await client.rag.collection.query(collection_id="my_unique_collection_id", request_query="What is this article about?")
+    return response
 
-# Extract structured data from a website
-extract_response = tools_client.extract(
-    website="https://www.forbes.com/real-time-billionaires/",
-    json_schema='[{"name": "<name>", "networth": "<networth>"}]'
-)
-print(extract_response)
+result = asyncio.run(main())
